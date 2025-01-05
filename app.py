d6ec474f7340b27312eb4e93c6a07cf8ac74dfdb
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-05 20:54:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown("""
         align-items: center;
     }
     .contact-links a img {
-        margin-right: 2px;
+        margin-right: 1px;
         width: 60px;
         height: 60px;
         border-radius: 50%;
