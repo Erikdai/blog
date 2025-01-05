4435d0cf203e079e5e0e0b6502a67f9280f530db
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-05 23:19:23 +0800

    Update certificates.py

--- pages/certificates.py
+++ pages/certificates.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="📜 Certificates",  # 设置页面的标题
+    page_title="📜Certificates",  # 设置页面的标题
     page_icon="📜",  # 设置页面的图标
 )
 st.title("Certificates 📜")
