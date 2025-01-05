b1a73415f135797fb76786063e53efca647f8045
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-05 21:01:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ with st.sidebar:
         )
 
 # Page content (Home page)
-st.title("About Me😎")
+st.title("About me: ")
 st.divider()
 
 # Main content section
@@ -138,7 +138,7 @@ with col2:
     st.markdown(
         """
         <div class="about-text" style="margin-top: -10px;"> <!-- 调整margin-top向上移动 -->
-        Hello! Welcome to my personal page :) I'm Dai, currently an Algorithm Engineer @ Imperial Vision and also a Research Intern @ the IFRC of Zhejiang University Binjiang Institute.
+        😎 Hello! Welcome to my personal page :) I'm Dai, currently an Algorithm Engineer @ Imperial Vision and also a Research Intern @ the IFRC of Zhejiang University Binjiang Institute.
         My areas of interest include <span style="font-weight:bold;">Machine Learning (ML)</span>, <span style="font-weight:bold;">Large Language Models (LLMs)</span>, and <span style="font-weight:bold;">Natural Language Processing (NLP)</span>, <span style="font-weight:bold;">Computer Vision (CV)</span>. I have also conducted some research in <span style="font-weight:bold;">AI for Social Sciences (Cognitive Computing)</span> and <span style="font-weight:bold;">signal Processing (Bearing Fault Detection)</span>.<br>
         </div>
         """,
