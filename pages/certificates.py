b848952ea697ed1871068c35d8fdfab6f8e66808
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-05 22:40:13 +0800

    Update and rename certificates.md to certificates.py

---
+++ pages/certificates.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+# 设置页面标题
+st.title("Certificates 📜")
+st.divider()
+
+# 使用 st.image 展示证书
+st.subheader("Certificate 1")
+st.image("./pages/cert1.png", caption="Certificate 1", use_column_width=True)
+
+st.subheader("Certificate 2")
+st.image("pages/cert2.png", caption="Certificate 2", use_column_width=True)
+
+st.subheader("Certificate 3")
+st.image("pages/cert3.png", caption="Certificate 3", use_column_width=True)
+
+st.subheader("Certificate 4")
+st.image("pages/cert4.png", caption="Certificate 4", use_column_width=True)
+
+st.subheader("Certificate 5")
+st.image("pages/cert5.png", caption="Certificate 5", use_column_width=True)
+
+st.subheader("Certificate 6")
+st.image("cert6.png", caption="Certificate 6", use_column_width=True)
+
+st.subheader("Certificate 7")
+st.image("cert7.png", caption="Certificate 7", use_column_width=True)
+
+st.subheader("Certificate 8")
+st.image("cert8.png", caption="Certificate 8", use_column_width=True)
+
+st.subheader("Certificate 9")
+st.image("cert9.png", caption="Certificate 9", use_column_width=True)
+
+st.subheader("Certificate 10")
+st.image("cert10.png", caption="Certificate 10", use_column_width=True)
+
+st.subheader("Certificate 11")
+st.image("cert11.png", caption="Certificate 11", use_column_width=True)
+
+st.subheader("Certificate 12")
+st.image("cert12.png", caption="Certificate 12", use_column_width=True)
+
+st.subheader("Certificate 13")
+st.image("cert13.png", caption="Certificate 13", use_column_width=True)
+
+st.subheader("Certificate 14")
+st.image("cert14.png", caption="Certificate 14", use_column_width=True)
