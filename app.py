f8a693eb4f1d841cb4a10ba08cf9610702a375e9
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-05 18:06:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ st.markdown(
             <img src="https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png" alt="LinkedIn">LinkedIn
         </a>
         <a href="mailto:daicxx1226@gmail.com">
-            <img src="https://www.pngarts.com/files/7/Google-Gmail-Logo-PNG-Picture.png" alt="Email">Email
+            <img src="https://freelogopng.com/images/all_img/1682570982gmail-name-logo.png" alt="Email">Email
         </a>
     </div>
     """,
