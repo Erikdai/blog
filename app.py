4e613d6795f85a782aaa4c6d7378b84d01f873ee
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-05 23:15:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,53 +7,51 @@ st.set_page_config(
     layout="wide",
 )
 
-# Sidebar navigation
-with st.sidebar:
-    st.header("Menu")
-    # Sidebar buttons
-    if "page" not in st.session_state:
-        st.session_state.page = "Home"  # Default to Home Page
+# Main Page (Home)
+st.title("About me")
+st.divider()
 
-    if st.button("🏠 Home"):
-        st.session_state.page = "Home"
-    if st.button("📜 Certificates"):
-        st.session_state.page = "Certificates"
+# Main content section
+st.markdown('<div class="main-content">', unsafe_allow_html=True)
 
-# 页面内容显示逻辑
-if st.session_state.page == "Home":
-    st.title("About me")
-    st.divider()
+# Columns for layout
+col1, col2 = st.columns([1, 2], gap="medium")
 
-    # Main content section
-    st.markdown('<div class="main-content">', unsafe_allow_html=True)
+# Left column for profile image
+with col1:
+    st.image("static/id.jpg", width=225, caption="DingDongJi")
 
-    # Columns for layout
-    col1, col2 = st.columns([1, 2], gap="medium")
+# Right column for detailed bio
+with col2:
+    st.markdown(
+        """
+        <div class="about-text" style="margin-top: -10px;">
+        😎 Hello! Welcome to my personal page :) I'm Dai, currently an Algorithm Engineer @ Imperial Vision and also a Research Intern @ the IFRC of Zhejiang University Binjiang Institute.
+        My areas of interest include <span style="font-weight:bold;">Machine Learning (ML)</span>, <span style="font-weight:bold;">Large Language Models (LLMs)</span>, and <span style="font-weight:bold;">Natural Language Processing (NLP)</span>, <span style="font-weight:bold;">Computer Vision (CV)</span>. I have also conducted some research in <span style="font-weight:bold;">AI for Social Sciences (Cognitive Computing)</span> and <span style="font-weight:bold;">Signal Processing (Bearing Fault Detection)</span>.
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
 
-    # Left column for profile image
-    with col1:
-        st.image("static/id.jpg", width=225, caption="DingDongJi")
+# End of main content section
+st.markdown(""" """)
+st.divider()
+st.markdown('</div>', unsafe_allow_html=True)
 
-    # Right column for detailed bio
-    with col2:
-        st.markdown(
-            """
-            <div class="about-text" style="margin-top: -10px;">
-            😎 Hello! Welcome to my personal page :) I'm Dai, currently an Algorithm Engineer @ Imperial Vision and also a Research Intern @ the IFRC of Zhejiang University Binjiang Institute.
-            My areas of interest include <span style="font-weight:bold;">Machine Learning (ML)</span>, <span style="font-weight:bold;">Large Language Models (LLMs)</span>, and <span style="font-weight:bold;">Natural Language Processing (NLP)</span>, <span style="font-weight:bold;">Computer Vision (CV)</span>. I have also conducted some research in <span style="font-weight:bold;">AI for Social Sciences (Cognitive Computing)</span> and <span style="font-weight:bold;">Signal Processing (Bearing Fault Detection)</span>.
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
-
-    # End of main content section
-    st.markdown(""" """)
-    st.divider()
-    st.markdown('</div>', unsafe_allow_html=True)
-
-elif st.session_state.page == "Certificates":
-    try:
-        import pages.certificates as certificates  # 动态加载 certificates.py
-        certificates.display_certificates()  # 调用 certificates.py 中的函数
-    except ModuleNotFoundError:
-        st.error("The file 'certificates.py' was not found.")
+# Footer with social links
+st.markdown(
+    """
+    <div class="contact-links">
+        <a href="https://www.kaggle.com/patpan" target="_blank">
+            <img src="https://www.dataapplab.com/wp-content/uploads/2016/10/kaggle-logo-transparent-300-768x349.png" alt="Kaggle" style="height: 45px; margin-right: 7px;">Kaggle
+        </a>
+        <a href="https://www.linkedin.com/in/chengxiaodai" target="_blank">
+            <img src="https://static.vecteezy.com/system/resources/previews/018/930/587/non_2x/linkedin-logo-linkedin-icon-transparent-free-png.png" alt="LinkedIn">LinkedIn
+        </a>
+        <a href="mailto:your_email@example.com">
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/018/972/241/small_2x/3d-message-icon-a-modern-email-concept-on-isolate-background-png.png" alt="Email" style="height: 45px; width: 45px; margin-right: 9px;">Email
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
