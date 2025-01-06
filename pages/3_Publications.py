052140dac90464c0446ecd2e5f656dd66cca460b
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-06 21:54:32 +0800

    Update 3_Publications.py

--- pages/3_Publications.py
+++ pages/3_Publications.py
@@ -15,7 +15,7 @@ publications = [
     {
         "title": "From the Perspective of Explainable Machine Learning: A Student Feature Selection Strategy Based on the Geometric Mean of Feature Importance and Robustness",
         "conference": "ACM ICCMT 2024 (Conference Paper, Published)",
-        "link": "YOUR_LINK_TO_PAPER",
+        "link": "https://dl.acm.org/doi/10.1145/3675249.3675335",
     },
     {
         "title": "Fault Diagnosis of Aero-engine Inter-shaft Bearings Using a Parallel SE-Depthwise Separable Convolutional Neural Network and Transformer Model",
@@ -24,7 +24,12 @@ publications = [
     {
         "title": "LawLuo: A Multi-Agent Collaborative Framework for Chinese Legal Consultation",
         "conference": "arXiv (Conference Paper/CCF-A, Under Review)",
-        "link": "YOUR_LINK_TO_PAPER",
+        "link": "https://arxiv.org/abs/2407.16252",
+    },
+    {
+        "title": "Study on breast cancer image detection and classification based on residual connected convolutional neural network (CNN)",
+        "conference": "2nd International Conference on Modern Medicine and Global Health (Conference Paper, Published)",
+        "link": "https://www.ewadirect.com/proceedings/tns/article/view/10586",
     },
     {
         "title": "Multimodal Large Language Model Enhancement Network for Multimodal Sentiment Analysis",
@@ -32,11 +37,11 @@ publications = [
     },
     {
         "title": "Multi-Stage Simulation of Residents' Perception and Decision Making Behavior Regarding Disaster Risk Information: An Exploratory Study on Large Language Model Agents Driven Social-Cognitive Simulation Framework",
-        "conference": "Journal Paper (JCR-Q1, Minor Revision)",
+        "conference": "Journal Paper (JCR-Q1, Under Minor Revision)",
     },
     {
         "title": "Swin Transformer with Large Margin Aware Focal Loss for Automatic Classification of Diabetic Retinopathy",
-        "conference": "Journal Paper (Scopus-Q3, Major Revision)",
+        "conference": "Journal Paper (Scopus-Q3, Under Major Revision)",
     },
 ]
 
