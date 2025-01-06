263fd7e4a46b1a8ab999f28082b7bd82acc84dda
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-06 22:08:58 +0800

    Update 3_Publications.py

--- pages/3_Publications.py
+++ pages/3_Publications.py
@@ -52,7 +52,7 @@ for idx, pub in enumerate(publications, start=1):
         f"""
         <div style="background-color: #f9f9f9; padding: 15px; border-radius: 10px; margin-bottom: 20px;">
             <h3>{pub['title']}</h3>
-             <p><b>Conference/Journal:</b> {pub['conference']} {"<a href='" + pub['link'] + "' target='_blank' text-decoration: none;'> 🔗 Read Full Text</a>" if 'link' in pub else ""}</p>
+             <p><b>Conference/Journal:</b> {pub['conference']} {"<a href='" + pub['link'] + "' target='_blank' text-decoration: none;'>Read Full Text🔗</a>" if 'link' in pub else ""}</p>
         </div>
         </div>
         """,
