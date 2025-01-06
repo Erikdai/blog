3eb9ad2ae7581ead294cdac40bd4d8e5ac8222a8
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-01-06 22:26:52 +0800

    Update 2_Experience.py

--- pages/2_Experience.py
+++ pages/2_Experience.py
@@ -17,7 +17,8 @@ st.markdown(
     """
     - **Universiti Tunku Abdul Rahman (UTAR)**  
       *Bachelor of Computer Science with Honours (Merit)*  
-      📅_2021-01 ~ 2024-09_    📍_Kampar, Perak, Malaysia_  
+      📅_2021-01 ~ 2024-09_    
+      📍_Kampar, Perak, Malaysia_  
     """
 )
 st.divider()
@@ -28,15 +29,18 @@ st.markdown(
     """
     - **Ruijie Networks**  
       *Technical Service Intern*  
-     📅 _2023-10 ~ 2024-01_    📍_Fuzhou, Fujian, China _  
+     📅 _2023-10 ~ 2024-01_    
+     📍_Fuzhou, Fujian, China _  
       
     - **Imperial Vision Group Co., Ltd., AI Research Institute**  
       *Algorithm Engineer Intern*  
-      📅_2024-12 ~ Present_    📍 _Fuzhou, Fujian, China _  
+      📅_2024-12 ~ Present_    
+      📍_Fuzhou, Fujian, China _  
       
     - **IFRC, Zhejiang University Binjiang Institute**  
       *Research Intern (Hybrid)*  
-      📅_2024-12 ~ Present_    📍 _Hangzhou, Zhejiang, China _  
+      📅_2024-12 ~ Present_    
+      📍_Hangzhou, Zhejiang, China _  
     """
 )
 st.divider()
