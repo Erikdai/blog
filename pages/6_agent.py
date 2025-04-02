eb8c834ca35344f1bd925e57527d2df4ca06bd6b
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-04-02 15:29:50 +0800

    Update 6_agent.py

--- pages/6_agent.py
+++ pages/6_agent.py
@@ -4,6 +4,6 @@ import streamlit.components.v1 as components
 st.title("出海智能助手 Demo")
 
 # 替换为你自己 Coze Bot 的 Web 公共链接
-iframe_url = "https://www.coze.cn/space/7488599222121086985/bot/7488600231509131318"  # 你的 Coze 应用网页地址
+iframe_url = "https://www.coze.cn/s/xTJTQFmVZwA/"  # 你的 Coze 应用网页地址
 
 components.iframe(iframe_url, height=600, scrolling=True)
