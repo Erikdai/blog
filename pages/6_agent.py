9549c9c2aaa82a4ae30144c848a69d0eb257e285
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2025-04-02 15:16:58 +0800

    Create 6_agent.py

---
+++ pages/6_agent.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title("我的出海智能助手")
+
+coze_html = """
+<div id="coze-widget"></div>
+<script>
+  window.cozeSettings = {
+    bot_id: '7488600231509131318',  // <-- 替换成你自己的
+    mode: 'popup',          // 或者 'embedded' 模式
+  };
+</script>
+<script src="https://cdn.coze.cn/web-sdk/v1/coze-web-sdk.umd.js"></script>
+"""
+
+components.html(coze_html, height=600)
