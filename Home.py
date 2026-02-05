eb077bc4d9dc29b54266287179973ab9f1550c2b
Dai,Cheng Xiao <106534091+Erikdai@users.noreply.github.com>
2026-02-05 19:36:26 +0800

    Adjust image width in profile section

--- Home.py
+++ Home.py
@@ -50,7 +50,7 @@ col1, col2 = st.columns([1, 2], gap="medium")
 
 # Left column for profile image
 with col1:
-    st.image("static/me.jpg", width=300, caption="Hi")
+    st.image("static/me.jpg", width=275, caption="Hi")
 
 # Right column for detailed bio
 with col2:
